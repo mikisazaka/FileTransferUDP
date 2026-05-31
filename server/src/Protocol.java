fb478faa623af187b44de75f8bff1f498dc891ea
Graciele Miki Sazaka <gracielesazaka@gmail.com>
2026-05-30 23:58:56 -0300

    feat: Adição da lógica do servidor

---
+++ server/src/Protocol.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Protocol {
+    public static byte[] receberBytes(DatagramSocket socket,
+                                      InetAddress ip,
+                                      int porta) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void enviarBytes(DatagramSocket socket,
+                                   InetAddress ip,
+                                   int porta,
+                                   byte[] dados) throws IOException {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ server/src/ServerMain.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class ServerMain {
+    private static final int PORTA = 9876;
+    private static final int TAMANHO_BUFFER = 1024;
+
+    public static void main(String[] args) {
+        System.out.println("Inicializando servidor UDP na porta " + PORTA + "...");
+
+        try (DatagramSocket serverSocket = new DatagramSocket(PORTA)) {
+            System.out.println("Servidor pronto e aguardando requisições...");
+
+            while (true) {
+                byte[] bufferRecebimento = new byte[TAMANHO_BUFFER];
+
+                DatagramPacket pacoteRecebido = new DatagramPacket(bufferRecebimento, bufferRecebimento.length);
+                serverSocket.receive(pacoteRecebido);
+
+                System.out.println("Nova requisição recebida de: "
+                        + pacoteRecebido.getAddress() + ":" + pacoteRecebido.getPort());
+
+                ClientHandler handler = new ClientHandler(pacoteRecebido);
+                Thread threadCliente = new Thread(handler);
+                threadCliente.start();
+            }
+
+        } catch (IOException e) {
+            System.err.println("Erro crítico no servidor: " + e.getMessage());
+        }
+    }
+}
